325243f4c28fc75c803ff0013d55b6a83d0704d5
Nithikrit Raibroycharoen <greninja028@Nithikrits-MacBook-Air.local>
2022-02-13 11:03:13 +0700

    update

--- code/support.py
+++ code/support.py
@@ -8,7 +8,6 @@ def import_csv_layout(path):
         layout = reader(level_map,delimiter = ',')
         for row in layout:
             terrain_map.append(list(row))
-
         return terrain_map
 
 def import_folder(path):
