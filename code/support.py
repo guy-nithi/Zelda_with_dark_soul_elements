e71c8129367d13da387307eeb0cb9ee4de402311
Nithikrit Raibroycharoen <greninja028@Nithikrits-MacBook-Air.local>
2022-02-27 15:12:47 +0700

    DONE

--- code/support.py
+++ code/support.py
@@ -14,6 +14,7 @@ def import_folder(path):
     surface_list = []
 
     for _,__,img_files in walk(path):
+        img_files.sort() # Sort file in directory thx to Klass & Daddy
         for image in img_files:
             full_path = path + '/' + image
             image_surf = pygame.image.load(full_path).convert_alpha()
